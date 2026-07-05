2ddb6a45054bb65cbc90d7ebcc9b9586e84af059
Ndung'u <phitahn@gmail.com>
2026-07-05 18:47:42 +0300

    clean initial commit

---
+++ src/data_ingestion.py
@@ -0,0 +1,24 @@
+# !kaggle datasets download -d kanjerih/credit-card-fraud-dataset
+
+import numpy as np
+import pandas as pd
+
+# +
+import zipfile
+
+with zipfile.ZipFile("credit-card-fraud-dataset.zip", 'r') as zip_ref:
+    zip_ref.extractall()
+# -
+
+import os
+os.listdir()
+
+data = pd.read_csv('creditcard.csv')
+data.head()
+
+<<<<<<< HEAD
+data.describe()
+
+=======
+>>>>>>> fab011c1f20e1986f42312e4f5d752c9ac63c8c7
+
