47c30bdb24a822b1a534a0a5b9320545f3dbd624
Ndung'u <phitahn@gmail.com>
2026-07-10 13:07:58 +0300

    src/data_preprocessing.py

--- src/data_preprocessing.py
+++ src/data_preprocessing.py
@@ -4,6 +4,26 @@ import numpy as np
 
 data = pd.read_csv('creditcard.csv')
 data.describe()
+# +
+import seaborn as sns
+
+sns.histplot(data.Time, kde=True)
+
+
+# +
+import seaborn as sns
+
+sns.histplot(data.Class, kde=True)
 # -
+
+data.Class
+
+data.Amount
+
+sns.histplot(data.Amount, kde =True
+            )
+
+sns.histplot(data.V14, kde =True
+            )
 
 
